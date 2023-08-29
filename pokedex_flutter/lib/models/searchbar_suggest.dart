162662a2a96273d5694cd022bf0c90a4eccbfbc2
Duwe <fernando_duwe@hotmail.com>
2023-08-29 20:24:42 -0300

    Ajustes na pesquisa

--- pokedex_flutter/lib/models/searchbar_suggest.dart
+++ pokedex_flutter/lib/models/searchbar_suggest.dart
@@ -1,9 +1,19 @@
 import 'package:pokedex_flutter/models/poke_model.dart';
 
 class SuggestionService {
-  static List<String> getPokeSuggestions(List<PokemonData> pokemons, String query) {
+  static List<String> getPokeSuggestions(
+      List<PokemonData> pokemons, String query) {
+    List<PokemonData> newList = [];
+
+    newList.addAll(pokemons);
+
+    newList.removeWhere((element) => !element.name.contains(query));
+
+    return newList.map((e) => e.name).toList();
+
+    /*
     return pokemons
         .map((pokemon) => pokemon.name)
-        .toList();
+        .toList();*/
   }
 }
